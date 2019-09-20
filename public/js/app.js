f8f8fcd567ab3d90f74ff6fe703bab0429f46ea1
mohedsahil <sahilmh19@gmail.com>
2019-09-21 00:54:10 +0530

    init commit

---
+++ public/js/app.js
@@ -0,0 +1,30 @@
+
+const weatherform = document.querySelector('form')
+const search = document.querySelector('input')
+const msg1 = document.getElementById('msg1')
+const msg2 = document.getElementById('msg2')
+
+
+weatherform.addEventListener('submit',(e)=>{
+//prevent from refresh
+e.preventDefault()
+const location = search.value
+msg1.textContent='Loading....'
+msg2.textContent=''
+
+const url = 'http://localhost:3000/weather?address='+location+''
+fetch(url).then((response)=>{       
+response.json().then((data)=>{
+    if(data.length==0){
+        msg1.textContent = 'please enter the location'
+    }
+    if(data.error){
+        msg1.textContent = data.error
+    }
+    else{
+        msg1.textContent = data.address
+        msg2.textContent = data.forecast
+    }
+})
+})
+})
